89d9baf2b70dd2be5fbae46eb40abf892a16a927
Thomas Werthmann <git@thomaswerthmann.name>
2020-05-26 22:15:40 +0200

    added dashboard config

---
+++ dashboard/download-and-patch-config.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+registry=registry.local:5000
+image_dashboard=${registry}/kubernetesui/dashboard:v2.0.1
+image_metrics_scraper=${registry}/kubernetesui/metrics-scraper:v1.0.4
+
+wget -O - https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.1/aio/deploy/recommended.yaml \
+ | sed -re 's#kubernetesui/dashboard:v2.0.1#'$image_dashboard'#' \
+ | sed -re 's#kubernetesui/metrics-scraper:v1.0.4#'$image_metrics_scraper'#' \
+ | sed -re 's#imagePullPolicy: .*$#imagePullPolicy: Never#' \
+ > dashboard.yaml
+
+patch < patch
