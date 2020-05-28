87c1eaafbacb4ccf94c8b048c222c86df1c09205
Thomas Werthmann <git@thomaswerthmann.name>
2020-05-28 21:21:45 +0200

    added script to automatically apply deployments after boot

--- 10_compile_ignition.sh
+++ 10_compile_ignition.sh
@@ -10,6 +10,7 @@ function dataurl_file() {
   echo "data:;base64,$(base64 -w0 $1)"
 }
 
+
 export MO_FAIL_ON_UNSET=true
 export CA_REGISTRY=$(dataurl_file tls/ca.crt)
 export SERVICEACCOUNT_KEY_PRIVATE=$(dataurl_file tls/k8s-service-accounts/private_unencrypted.pem)
@@ -17,6 +18,10 @@ export SERVICEACCOUNT_KEY_PUBLIC=$(dataurl_file tls/k8s-service-accounts/public.
 export K8S_CA_CRT=$(dataurl_file tls/k8s-api-server/ca.crt)
 export K8S_APISERVER_CRT=$(dataurl_file tls/k8s-api-server/certs/apiserver/crt)
 export K8S_APISERVER_KEY=$(dataurl_file tls/k8s-api-server/certs/apiserver/key)
+
+# tar.xz created from the deployments directory
+deployments-builder/build-all.sh
+export K8S_DEPLOYMENTS_TAR_XZ="data:;base64,"$(tar -cJf - -C deployments/ ./ | base64 -w0)
 
 cat example.fcc \
   | mo \
