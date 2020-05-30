3015979c98b6b16120beb110ac95f51282d2e261
Thomas Werthmann <git@thomaswerthmann.name>
2020-05-30 23:27:41 +0200

    removed mo from the actual path to compile the ignition file - leave it there because it might be handy to get a configurable config file

--- 10_compile_ignition.sh
+++ 10_compile_ignition.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-source $(dirname $0)/bin/mo/mo
+#source $(dirname $0)/bin/mo/mo
 
-function dataurl() {
-  echo "data:;base64,$(echo "$1" | base64 -w0)"
-}
+#function dataurl() {
+#  echo "data:;base64,$(echo "$1" | base64 -w0)"
+#}
 
-function dataurl_file() {
-  echo "data:;base64,$(base64 -w0 $1)"
-}
+#function dataurl_file() {
+#  echo "data:;base64,$(base64 -w0 $1)"
+#}
 
 
-export MO_FAIL_ON_UNSET=true
-
+#export MO_FAIL_ON_UNSET=true
+#cat example.fcc \
+#  | mo \
+#  | docker run -i --rm quay.io/coreos/fcct:release --strict \
+#  > example.ign
 
 cat example.fcc \
-  | mo \
   | docker run -i --rm quay.io/coreos/fcct:release --strict \
   > example.ign
 
